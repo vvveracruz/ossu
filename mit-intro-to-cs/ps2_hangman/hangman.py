f65081d035a903ae9e2ca499e579ecf5611fb7c9
vvveracruz <login@vgg.cz>
2021-06-12 09:39:04 +0100

    finished pset2 problem2

--- mit-intro-to-cs/ps2_hangman/hangman.py
+++ mit-intro-to-cs/ps2_hangman/hangman.py
@@ -53,10 +53,41 @@ def get_guessed_word(secret_word, letters_guessed):
         comprised of letters, underscores (_), and spaces that represents
         which letters in secret_word have been guessed so far.
     '''
+    # return letters and underscores representing which letters have been guessed so far
+    guessed_word = ''
+    delimiter='_'
+
+    for char in secret_word:
+        if char in letters_guessed:
+            guessed_word+=char
+        else:
+            guessed_word+=delimiter
+        guessed_word+=' '
+    
+    return guessed_word
+
+def get_available_letters(letters_guessed):
+    '''
+    letters_guessed: list (of letters), 
+        which letters have been guessed so far
+    returns: string (of letters), 
+        comprised of letters that represents which letters have not
+        yet been guessed.
+    '''
+    all_lowercase = string.ascii_lowercase
+    available_letters = ''
+
+    for char in all_lowercase:
+        if char in letters_guessed:
+            pass
+        else:
+            available_letters+=char
+    
+    return available_letters
 
 if __name__=="__main__":
     # secret_word = choose_word()
     secret_word = 'apple'
-    letters_guessed = 'lae'
+    letters_guessed = 'ale'
 
-    print(is_word_guessed(secret_word, letters_guessed))
+    print(get_available_letters(letters_guessed))
