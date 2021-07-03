497b312629fcd52adb6feb1fc7a75f7d21fc2f43
vvveracruz <login@vgg.cz>
2021-07-03 13:13:10 +0100

    finished pset 2

--- mit-intro-to-cs/ps2_hangman/hangman.py
+++ mit-intro-to-cs/ps2_hangman/hangman.py
@@ -95,11 +95,42 @@ def get_update_message(guesses, letters_guessed):
     print('Available letters: '+get_available_letters(letters_guessed)+'.')
     return None
     
+def match_with_gaps(guessed_word, other_word):
+    # checks whether guessed_word could be other_word
+    guessed_word = guessed_word.replace(' ', '')
+    if len(guessed_word) != len(other_word):
+        return False
+
+    letters_guessed = ''.join(set(guessed_word)) # unique letters in secret word
+
+    for i in range(len(guessed_word)):
+        if guessed_word[i]==other_word[i]:
+            pass
+        elif guessed_word[i]=='_' and other_word[i] not in letters_guessed:
+            pass
+        else:
+            return False
+
+    return True
+
+def show_possible_matches(guessed_word):
+    L = []
+    for word in _WORDLIST:
+        if match_with_gaps(guessed_word, word):
+            L.append(word)
+    if len(L)==0:
+        return 'No matches found.'
+    else:
+        return L
+        
 def display_loss(secret_word):
     print('\nSorry, you lost. The secret word was', secret_word+'.\n\n')
 
-def display_win():
-    print('\nNice! YOU WON!!!!\n\n')
+def display_win(score):
+    print(f'\nNice, you won. \nYou scored {score}\n')
+
+def show_hint(guessed_word):
+    print('Possible word matches are:\n', show_possible_matches(guessed_word))
 
 def play(secret_word):
     guesses = math.ceil(len(secret_word))
@@ -114,25 +145,29 @@ def play(secret_word):
     # main loop
     while guesses != 0:
         get_update_message(guesses, letters_guessed)
+        guessed_word = get_guessed_word(secret_word, letters_guessed)
         guess = input('Please guess a letter: ')
+        guess = guess.lower()
         letters_guessed += guess
+        if guess == '*':
+            show_hint(guessed_word)
         while len(guess) != 1 or guess not in string.ascii_lowercase:
-            guess = input('Input not valid.\nPlease guess a lowercase letter: ')
+            guess = input('Please guess a one single lowercase letter: ')
         if guess in secret_word:
             print('Good guess!')
             print(get_guessed_word(secret_word, letters_guessed))
         else:
             guesses -= 1
-        if secret_word == get_guessed_word(secret_word, letters_guessed).replace(' ', ''):
-                display_win()
+        guessed_word = get_guessed_word(secret_word, letters_guessed)
+        score = guesses * len(''.join(set(secret_word))) # unique letters in secret word
+        if secret_word == guessed_word.replace(' ', ''):
+                display_win(score)
                 break
         if guesses == 0:
             display_loss(secret_word)
-        
-
 
 if __name__=="__main__":
     secret_word = choose_word()
-    # secret_word = 'apple'
+    #secret_word = 'apple'
 
     play(secret_word)
